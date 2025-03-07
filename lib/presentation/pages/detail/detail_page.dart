a6d8b9c8406285cbc8b21feaa7d8475bcf84d507
TaeBbong <mok05289@korea.ac.kr>
2025-03-07 15:22:08 +0900

    wip: detail page ui

--- lib/presentation/pages/detail/detail_page.dart
+++ lib/presentation/pages/detail/detail_page.dart
@@ -1,1 +1,23 @@
 // TODO: 일정 편집 기능(폼) / 축의금 금액 입력(db 필드 추가 및 업데이트)
+
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatefulWidget {
+  const DetailPage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
