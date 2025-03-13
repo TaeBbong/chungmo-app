f739dc25d03ec29de6e2a7cf4ad4a1a7591c9948
TaeBbong <mok05289@korea.ac.kr>
2025-03-13 10:41:29 +0900

    fix: don't add schedule notification before now

--- lib/core/services.dart/notification_service.dart
+++ lib/core/services.dart/notification_service.dart
@@ -10,6 +10,9 @@ class NotificationService {
   static final FlutterLocalNotificationsPlugin _localNotifyPlugin =
       FlutterLocalNotificationsPlugin();
 
+  /// Get permissions from user when opens app.
+  ///
+  /// Called by main.dart
   static Future<void> getPermissions() async {
     if (await Permission.notification.isDenied &&
         !await Permission.notification.isPermanentlyDenied) {
@@ -17,6 +20,9 @@ class NotificationService {
     }
   }
 
+  /// Initialize notification plugin settings.
+  ///
+  /// Called by main.dart
   static Future<void> init() async {
     AndroidInitializationSettings android =
         const AndroidInitializationSettings("@mipmap/ic_launcher");
@@ -31,6 +37,12 @@ class NotificationService {
     tz.initializeTimeZones();
   }
 
+  /// Add notification at calculated date(_timeZoneSetting).
+  ///
+  /// If calculated date is before now, doesn't make notification.
+  /// If calculated date is now but time is over 11:00am, notify after 1 hour.
+  ///
+  /// Called by ScheduleRepository; when user create/edit schedule.
   static Future<void> notifyScheduleAtPreviousDay({
     required Schedule schedule,
   }) async {
@@ -48,6 +60,21 @@ class NotificationService {
       ),
     );
 
+    tz.TZDateTime scheduleDate =
+        _timeZoneSetting(scheduleDate: schedule.date, hour: 11, minute: 0);
+
+    final now = tz.TZDateTime.now(scheduleDate.location);
+    final todayEleven = tz.TZDateTime(
+        scheduleDate.location, now.year, now.month, now.day, 11, 0);
+
+    if (scheduleDate.isAtSameMomentAs(todayEleven) &&
+        now.isBefore(todayEleven)) {
+      scheduleDate = tz.TZDateTime(
+          scheduleDate.location, now.year, now.month, now.day, now.hour + 1, 0);
+    } else if (scheduleDate.isBefore(todayEleven)) {
+      return;
+    }
+
     final int id = await UrlHash.hashUrlToInt(schedule.link);
 
     await _localNotifyPlugin.zonedSchedule(
@@ -59,14 +86,21 @@ class NotificationService {
       uiLocalNotificationDateInterpretation:
           UILocalNotificationDateInterpretation.absoluteTime,
       androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
+      payload:
+          _timeZoneSetting(scheduleDate: schedule.date, hour: 11, minute: 0)
+              .toIso8601String(),
     );
   }
 
+  /// Cancel notification schedule already added.
+  ///
+  /// Called by ScheduleRepository when user edit/delete schedule.
   static Future<void> cancelNotifySchedule({required String link}) async {
     final int id = await UrlHash.hashUrlToInt(link);
     await _localNotifyPlugin.cancel(id);
   }
 
+  /// Calculate targetDate = scheduleDate - 1, so user can get notification on day before event.
   static tz.TZDateTime _timeZoneSetting({
     required String scheduleDate,
     required int hour,
@@ -89,7 +123,7 @@ class NotificationService {
 
     for (var notification in pendingNotifications) {
       print(
-          "🔔 ID: ${notification.id}, Title: ${notification.title}, Body: ${notification.body}");
+          "🔔 ID: ${notification.id}, Title: ${notification.title}, Body: ${notification.body}, Date: ${notification.payload}");
     }
   }
 }
