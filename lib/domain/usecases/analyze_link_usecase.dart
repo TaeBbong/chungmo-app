d120c28c9c85668daac0f897748a3baf81691052
TaeBbong <mok05289@korea.ac.kr>
2025-05-02 17:17:21 +0900

    refact: apply BaseUsecase

--- lib/domain/usecases/analyze_link_usecase.dart
+++ lib/domain/usecases/analyze_link_usecase.dart
@@ -5,15 +5,17 @@
 
 import 'package:injectable/injectable.dart';
 
+import '../../core/base/base_usecase.dart';
 import '../entities/schedule.dart';
 import '../repositories/schedule_repository.dart';
 
 @injectable
-class AnalyzeLinkUsecase {
+class AnalyzeLinkUsecase implements ParamUsecase<String, Future<Schedule>> {
   final ScheduleRepository repository;
 
   AnalyzeLinkUsecase(this.repository);
 
+  @override
   Future<Schedule> execute(String link) {
     return repository.analyzeLink(link);
   }
--- lib/domain/usecases/save_schedule_usecase.dart
+++ lib/domain/usecases/save_schedule_usecase.dart
@@ -5,15 +5,17 @@
 
 import 'package:injectable/injectable.dart';
 
+import '../../core/base/base_usecase.dart';
 import '../entities/schedule.dart';
 import '../repositories/schedule_repository.dart';
 
 @injectable
-class SaveScheduleUsecase {
+class SaveScheduleUsecase implements ParamUsecase<Schedule, Future<void>> {
   final ScheduleRepository repository;
 
   SaveScheduleUsecase(this.repository);
 
+  @override
   Future<void> execute(Schedule schedule) {
     return repository.saveSchedule(schedule);
   }
