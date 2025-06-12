9cc88410fadf640c68de2a8039b0e225dab7162a
TaeBbong <mok05289@korea.ac.kr>
2025-06-12 22:33:19 +0900

    feat: implement calendar, create, detail bloc&cubit instead of GetxController

---
+++ lib/presentation/bloc/calendar/calendar_state.dart
@@ -0,0 +1,49 @@
+import 'package:equatable/equatable.dart';
+import '../../../domain/entities/schedule.dart';
+
+class CalendarState extends Equatable {
+  final bool isLoading;
+  final DateTime focusedDay;
+  final DateTime selectedDay;
+  final List<Schedule> allSchedules;
+  final Map<DateTime, List<Schedule>> currentMonthSchedules;
+
+  const CalendarState({
+    required this.isLoading,
+    required this.focusedDay,
+    required this.selectedDay,
+    required this.allSchedules,
+    required this.currentMonthSchedules,
+  });
+
+  factory CalendarState.initial() {
+    return CalendarState(
+      isLoading: false,
+      focusedDay: DateTime.now(),
+      selectedDay: DateTime.now(),
+      allSchedules: const [],
+      currentMonthSchedules: const {},
+    );
+  }
+
+  CalendarState copyWith({
+    bool? isLoading,
+    DateTime? focusedDay,
+    DateTime? selectedDay,
+    List<Schedule>? allSchedules,
+    Map<DateTime, List<Schedule>>? currentMonthSchedules,
+  }) {
+    return CalendarState(
+      isLoading: isLoading ?? this.isLoading,
+      focusedDay: focusedDay ?? this.focusedDay,
+      selectedDay: selectedDay ?? this.selectedDay,
+      allSchedules: allSchedules ?? this.allSchedules,
+      currentMonthSchedules:
+          currentMonthSchedules ?? this.currentMonthSchedules,
+    );
+  }
+
+  @override
+  List<Object?> get props =>
+      [isLoading, focusedDay, selectedDay, allSchedules, currentMonthSchedules];
+}
